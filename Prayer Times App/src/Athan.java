dd2c3872bcb03f6a76b370bef5602e6081036a66
Hesham Awadalla <hesham.awadalla@yahoo.com>
2023-04-30 15:45:28 +0200

    #Start

---
+++ Prayer Times App/src/Athan.java
@@ -0,0 +1,25 @@
+import java.awt.EventQueue;
+
+public class Athan
+{
+
+	//Launch the application:
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
